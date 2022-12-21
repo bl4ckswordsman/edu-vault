5d20bd826b750e9463cd05dfe315a534e7b3c206
amra2101 <amarildo.rajta@gmail.com>
2022-12-21 17:54:49 +0000

    jumping works

--- main.cpp
+++ main.cpp
@@ -7,36 +7,45 @@
 #include "gameplay.h"
 #include "C12832.h"
 //#include "LEDs.h"
+    int dinoX = 2;
+    int dinoY = 17;
 
 C12832 lcd(p5, p7, p6, p8, p11);
 
 InterruptIn center2(p14);
-InterruptIn up(p15);
+DigitalIn up(p15);
 bool centerPressed = false;
 
 void centerPress() {
   centerPressed=true;
 }
 
-void jump(int &y) {
-    for (int i=1; i<16; i++){
-        y-=1;
+void jump() {
+    for (int i=1; i<4; i++){
+        dinoY-=4;
         wait(0.2);
+        lcd.cls();
+        printBackG(lcd, centerPressed);
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
     }
-    for (int i=1; i<16; i++){
-        y+=1;
+    for (int i=1; i<4; i++){
+        dinoY+=4;
         wait(0.2);
+        lcd.cls();
+        printBackG(lcd, centerPressed);
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
     }
 }
 
 
 int main() {
-    int dinoX = 2;
-    int dinoY = 17;
+
   // allLEDs();
 
   center2.fall(&centerPress);
-  up.fall(jump(dinoY));
+  //up.fall(&jump);
 
   while (1) {
     if (centerPressed) {
@@ -45,6 +54,46 @@ int main() {
     } else {
       printStartS(lcd, centerPressed);
     }
+
+    if (up){
+        jump();
+    }
+        /*dinoY-=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+        dinoY-=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+        dinoY-=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+        dinoY-=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+
+        dinoY+=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+        dinoY+=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+        dinoY+=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+        dinoY+=4;
+        printDino(lcd, centerPressed, dinoX, dinoY);
+        lcd.copy_to_lcd();
+        wait(0.2);
+
+    }*/
+
     wait(0.01);
     lcd.copy_to_lcd();
   }

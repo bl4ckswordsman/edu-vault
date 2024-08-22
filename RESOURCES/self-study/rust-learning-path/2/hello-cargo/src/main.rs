d56096e02195c0364b57c99967c323b47ea2bbf4
amarildo <amarildo.rajta@gmail.com>
2024-08-22 17:08:18 +0200

    add cargo option of running rust  apps

---
+++ RESOURCES/self-study/rust-learning-path/2/hello-cargo/src/main.rs
@@ -0,0 +1,9 @@
+/*
+Create project with cargo:      cargo new hello-cargo
+                                cd hello-cargo
+                                cargo run
+*/
+
+fn main() {
+    println!("Hello, world!");
+}

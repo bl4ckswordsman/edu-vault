c2b1433ccea8c1e2e4ce9ef3dcaa53f2f06b91c0
amarildo <amarildo.rajta@gmail.com>
2024-08-23 16:08:52 +0200

    restructure directories

--- RESOURCES/self-study/rust-learning-path/3/2-variables/src/main.rs
+++ RESOURCES/self-study/rust-learning-path/3/2-variables/src/main.rs
@@ -21,4 +21,18 @@ fn main() {
     // Change the value of an immutable variable
     a_number_new = 15;
     println!("Now the number(mut) is {}.", a_number_new);
+
+    println!("------------");
+    // Variable shadowing
+
+    // Declare first variable binding with name "shadow_num"
+    let shadow_num = 5;
+
+    // Declare second variable binding, shadows existing variable "shadow_num"
+    let shadow_num = shadow_num + 5;
+
+    // Declare third variable binding, shadows second binding of variable "shadow_num"
+    let shadow_num = shadow_num * 2;
+
+    println!("The number is {}.", shadow_num);
 }
